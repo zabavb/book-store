00ea581208a08d3f41bd0e007ed4fc1f04f2d044
Admin <Admin@DESKTOP-HEOO8JS>
2024-12-05 00:29:21 +0200

    category class added

---
+++ Library/BookEntities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.BookEntities
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+
+}
