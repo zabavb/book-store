75ea1bcaeb077a0e05c5d90b2da3c56d08d091f9
Admin <Admin@DESKTOP-HEOO8JS>
2024-12-05 00:30:15 +0200

    covertype enum added

---
+++ Library/BookEntities/CoverType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.BookEntities
+{
+    public enum CoverType
+    {
+        [Display(Name = "М'яка")]
+        SoftCover,
+
+        [Display(Name = "Тверда")]
+        Hardcover,
+
+        [Display(Name = "На спіралі")]
+        RingBinding,
+
+        [Display(Name = "Шкіряна")]
+        Leather,
+
+        [Display(Name = "Суперобкладинка")]
+        DustJacket
+    }
+
+}
