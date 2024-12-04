c89d01d9cae0533c6294807905c24d6a2b3fb7c5
Admin <Admin@DESKTOP-HEOO8JS>
2024-12-05 00:30:15 +0200

    covertype enum added

---
+++ Library/BookEntities/Language.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.BookEntities
+{
+    public enum Language
+    {
+        [Display(Name = "Англійська")]
+        English,
+
+        [Display(Name = "Українська")]
+        Ukrainian,
+
+        [Display(Name = "Іспанська")]
+        Spanish,
+
+        [Display(Name = "Французька")]
+        French,
+
+        [Display(Name = "Німецька")]
+        German,
+
+        [Display(Name = "Other")]
+        Other
+    }
+
+
+}
