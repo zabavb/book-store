a927fc4fe02aed7446a1b064ce87e28eed912805
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-07 16:27:20 +0200

    Orders controllers created

---
+++ OrderApi/Controllers/OrdersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderApi.Services;
+
+namespace OrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrders()
+        {
+            return null;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderDto>> GetOrderById(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<OrderDto>> CreateOrder(OrderDto orderDto)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<OrderDto>> UpdateOrder(Guid id, OrderDto orderDto)
+        {
+            return null;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteOrder(Guid id)
+        {
+            return null;
+        }
+    }
+}
