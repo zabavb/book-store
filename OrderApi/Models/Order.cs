c3aeda476710889d86cea181c12899a9cda9648f
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-07 15:45:45 +0200

    Added Order class model

---
+++ OrderApi/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace Library.OrderEntities
+{
+    internal class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public List<Guid> BookIds { get; set; } = new List<Guid>();
+
+        public string Region { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Address { get; set; } = null!;
+
+        public float Price { get; set; }
+        public DeliveryType Delivery { get; set; }
+        public float DeliveryPrice { get; set; }
+        public DateTime DeliveryDate { get; set; } = DateTime.Now;
+        public DateTime DeliveryTime { get; set; }  // + 2 days, in case of Nova Post
+
+    }
+}
