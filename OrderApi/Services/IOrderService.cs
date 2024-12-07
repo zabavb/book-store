b662c1d60f50de42f2d4141e3962d36a1165cc95
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-07 16:22:23 +0200

    Added OrderService interface

---
+++ OrderApi/Services/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderApi.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderDto>> GetOrdersAsync();
+        Task<OrderDto> GetOrderByIdAsync(Guid orderId);
+        Task<OrderDto> CreateOrderAsync(OrderDto orderDto);
+        Task<OrderDto> UpdateOrderAsync(Guid id, OrderDto orderDto);
+        Task<bool> DeleteOrderAsync(Guid id);
+    }
+}
