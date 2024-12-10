1805f7a518af6e770b124afb75a594e4d0060ca1
Mariia Kandyba <mariia.kandyba@gmail.com>
2024-12-10 18:52:56 +0200

    ICategoryService interface described

--- BookApi/Services/ICategoryService.cs
+++ BookApi/Services/ICategoryService.cs
@@ -2,5 +2,10 @@
 {
     public interface ICategoryService
     {
+        Task<IEnumerable<CategoryDto>> GetCategoriesAsync();
+        Task<CategoryDto> GetCategoryByIdAsync(Guid id);
+        Task<CategoryDto> CreateCategoryAsync(CategoryDto CategoryDto);
+        Task<CategoryDto> UpdateCategoryAsync(Guid id, CategoryDto CategoryDto);
+        Task<bool> DeleteCategoryAsync(Guid id);
     }
 }
