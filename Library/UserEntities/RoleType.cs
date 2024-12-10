c167628d09e605ed4bd78f8cde5b8cd0c6e8cf67
Viktor <bilonizkavik@gmail.com>
2024-12-10 14:25:54 +0100

    Added enum RoleType.

---
+++ Library/UserEntities/RoleType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.UserEntities
+{
+    public enum RoleType
+    {
+        [Display(Name = "admin")]
+        ADMIN,
+        [Display(Name = "moderator")]
+        MODERATOR,
+        [Display(Name = "user")]
+        USER,
+        [Display(Name = "guest")]
+        GUEST
+    }
+}
