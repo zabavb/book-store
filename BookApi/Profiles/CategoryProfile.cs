17d6aa35816249ca68c71dbc3ad72534146e8c4d
Mariia Kandyba <mariia.kandyba@gmail.com>
2024-12-10 18:22:02 +0200

    CategoryProfile created

---
+++ BookApi/Profiles/CategoryProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Library.BookEntities;
+
+namespace BookApi.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>()
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+
+            CreateMap<CategoryDto, Category>()
+                .ForMember(dest => dest.CategoryId, opt => opt.Ignore());
+        }
+    }
+}
