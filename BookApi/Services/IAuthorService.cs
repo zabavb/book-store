310d0f9587b8e1e13cad6fbb955280dce70d04a5
Mariia Kandyba <mariia.kandyba@gmail.com>
2024-12-10 17:50:40 +0200

    IAuthorService created

---
+++ BookApi/Services/IAuthorService.cs
@@ -0,0 +1,11 @@
+﻿namespace BookApi.Services
+{
+    public interface IAuthorService
+    {
+        Task<IEnumerable<AuthorDto>> GetAllAuthorsAsync();
+        Task<AuthorDto> GetAuthorByIdAsync(Guid authorId);
+        Task<AuthorDto> CreateAuthorAsync(AuthorDto authorDto);
+        Task<bool> UpdateAuthorAsync(Guid authorId, AuthorDto authorDto);
+        Task<bool> DeleteAuthorAsync(Guid authorId);
+    }
+}
