5d3842b1134ec7236116507f279bd20aa57f81f4
Mariia Kandyba <mariia.kandyba@gmail.com>
2024-12-10 17:49:57 +0200

    DateOfBirth added to the local BookApi Model

--- BookApi/Models/Author.cs
+++ BookApi/Models/Author.cs
@@ -5,6 +5,8 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string? Biography { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
 
         internal ICollection<Book> Books { get; set; } = new List<Book>();
     }
