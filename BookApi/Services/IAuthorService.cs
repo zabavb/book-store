540fe6f91cb2f32a355f4ce7d0c24716967c0afc
Mariia Kandyba <mariia.kandyba@gmail.com>
2024-12-10 17:57:08 +0200

    return type and method name updated

--- BookApi/Services/IAuthorService.cs
+++ BookApi/Services/IAuthorService.cs
@@ -3,9 +3,9 @@
     public interface IAuthorService
     {
         Task<IEnumerable<AuthorDto>> GetAllAuthorsAsync();
-        Task<AuthorDto> GetAuthorByIdAsync(Guid authorId);
+        Task<AuthorDto> GetAuthorByIdAsync(Guid id);
         Task<AuthorDto> CreateAuthorAsync(AuthorDto authorDto);
-        Task<bool> UpdateAuthorAsync(Guid authorId, AuthorDto authorDto);
-        Task<bool> DeleteAuthorAsync(Guid authorId);
+        Task<AuthorDto> UpdateAuthorAsync(Guid id, AuthorDto authorDto);
+        Task<bool> DeleteAuthorAsync(Guid id);
     }
 }
