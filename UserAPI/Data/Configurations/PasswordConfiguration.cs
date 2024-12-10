c0e3ce83991f5e3d4b5462efef3543a32502ca4e
Viktor <bilonizkavik@gmail.com>
2024-12-10 21:24:29 +0100

    Implemented configuration files for existing user entities, also wrote some additional validations for database.

---
+++ UserAPI/Data/Configurations/PasswordConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Library.UserEntities;
+
+namespace UserAPI.Data.Configurations
+{
+    public class PasswordConfiguration : IEntityTypeConfiguration<Password>
+    {
+        public void Configure(EntityTypeBuilder<Password> builder)
+        {
+            builder.Property(p => p.PasswordId)
+                .HasDefaultValueSql("NEWSEQUENTIALID()");
+        }
+    }
+}
