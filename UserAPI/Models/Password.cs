3e6c31fc5b1d690a45cfa028ac96ff27761b0b6c
Viktor <bilonizkavik@gmail.com>
2024-12-10 21:22:52 +0100

    Described Password model with validations.

---
+++ UserAPI/Models/Password.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserAPI.Models
+{
+    public class Password
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required, MaxLength(30), DataType(DataType.Text)]
+        public string PasswordHash { get; set; }
+        [Required, MaxLength(4), DataType(DataType.Text)]
+        public string PasswordSalt { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [NotMapped]
+        public UserDTO? User { get; set; } = new();
+
+        public Password()
+        {
+            PasswordHash = string.Empty;
+            PasswordSalt = string.Empty;
+        }
+    }
+}
