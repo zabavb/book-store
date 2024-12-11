c13bf0f2a138fe151291b9c8e67829100eda49cb
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-11 19:17:54 +0200

    Added basic validators

--- Library/OrderEntities/Order.cs
+++ Library/OrderEntities/Order.cs
@@ -1,19 +1,35 @@
-﻿namespace Library.OrderEntities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.OrderEntities
 {
     public class Order
     {
+        [Required]
         public Guid OrderId { get; set; }
+        [Required]
         public Guid UserId { get; set; }
+        [Required]  
         public List<Guid> BookIds { get; set; } = new List<Guid>();
 
+        [Required]
+        [StringLength(20)]
         public string Region { get; set; } = string.Empty;
+        [Required]
+        [StringLength(20)]
         public string City { get; set; } = string.Empty;
+        [Required]
+        [StringLength(20)]
         public string Address { get; set; } = string.Empty;
 
+        [Required]
         public float Price { get; set; }
+        [Required]
         public DeliveryType Delivery { get; set; }
+        [Required]
         public float DeliveryPrice { get; set; }
+        [Required]
         public DateTime DeliveryDate { get; set; } = DateTime.Now;
+        [Required]
         public DateTime DeliveryTime { get; set; }  // + 2 days, in case of Nova Post
 
         public OrderStatus Status { get; set; } = OrderStatus.RECEIVED;
