f9b86637911edd8ee66356256af5cadb64abc29b
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-11 19:20:35 +0200

    Enumerator validator created

---
+++ Library/Validators/EnumRangeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Validators
+{
+    public class EnumRangeAttribute : ValidationAttribute
+    {
+        private readonly Type _enumType;
+
+        public EnumRangeAttribute(Type enumType)
+        {
+            if (!enumType.IsEnum)
+            {
+                throw new ArgumentException("Type must be an enum");
+            }
+            _enumType = enumType;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            return Enum.GetNames(_enumType).Contains(value.ToString());
+        }
+    }
+}
