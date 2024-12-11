7f15cc5bb48619458ed3d1d3e338582359f0b6aa
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-11 19:36:34 +0200

    Redacted StringLength validator's values

--- Library/OrderEntities/Order.cs
+++ Library/OrderEntities/Order.cs
@@ -13,21 +13,23 @@ namespace Library.OrderEntities
         public List<Guid> BookIds { get; set; } = new List<Guid>();
 
         [Required]
-        [StringLength(20)]
+        [StringLength(100, ErrorMessage = "Region must be less than 100 characters.")]
         public string Region { get; set; } = string.Empty;
         [Required]
-        [StringLength(20)]
+        [StringLength(100, ErrorMessage = "City must be less than 100 characters.")]
         public string City { get; set; } = string.Empty;
         [Required]
-        [StringLength(20)]
+        [StringLength(255,ErrorMessage = "Address must be less than 255 characters.")]
         public string Address { get; set; } = string.Empty;
 
         [Required]
+        [Range(0, float.MaxValue)]
         public float Price { get; set; }
         [Required]
         [EnumRange(typeof(DeliveryType))]
         public DeliveryType Delivery { get; set; }
         [Required]
+        [Range(0, float.MaxValue)]
         public float DeliveryPrice { get; set; }
         [Required]
         public DateTime DeliveryDate { get; set; } = DateTime.Now;
