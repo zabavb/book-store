e517e4ee88cb9f57b3963c152d9938e6f60fd273
Viktor <bilonizkavik@gmail.com>
2024-12-11 10:24:26 +0100

    Added property EndDate with constructors to Subscription entity.

--- Library/UserEntities/Subscription.cs
+++ Library/UserEntities/Subscription.cs
@@ -8,8 +8,23 @@ namespace Library.UserEntities
 {
     public class Subscription
     {
-        public Guid SubscriptionId { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public Guid? SubscriptionId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Subscription()
+        {
+            Title = string.Empty;
+            Description = string.Empty;
+            EndDate = DateTime.Now.AddDays(1);
+        }
+        public Subscription(Guid? subscriptionId, string title, string description, DateTime endDate)
+        {
+            SubscriptionId = subscriptionId;
+            Title = title;
+            Description = description;
+            EndDate = endDate;
+        }
     }
 }
