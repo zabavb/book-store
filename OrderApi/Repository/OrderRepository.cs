b6f017eaae68e5002a1df563f2e4d2a4e5c76ce6
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-12 20:49:51 +0200

    Implemented IOrderRepository in OrderRepository class

---
+++ OrderApi/Repository/OrderRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApi.Data;
+using OrderApi.Models;
+
+namespace Client.Repository
+{
+    internal class OrderRepository : IOrderRepository
+    {
+        private readonly OrderDbContext _context;
+
+        public OrderRepository(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Order>> GetAllAsync()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order?> GetByIdAsync(Guid id)
+        {
+            return await _context.Orders.FirstOrDefaultAsync(o => o.Id == id);
+        }
+
+        public async Task AddAsync(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Order order)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Order order)
+        {
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+}
