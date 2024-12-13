f9117a6c7af3bbfae8f41fb9cd00b925a13d75f2
Viktor <bilonizkavik@gmail.com>
2024-12-13 14:32:55 +0100

    Implemented Subscription and User profiles for mappinf between original entity and it's DTO

---
+++ UserAPI/Profiles/SubscriptionProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UserAPI.Models.DTOs;
+
+namespace UserAPI.Profiles
+{
+    public class SubscriptionProfile : Profile
+    {
+        public SubscriptionProfile() {
+            CreateMap<Subscription, SubscriptionDTO>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.SubscriptionId))
+                .ReverseMap()
+                .ForMember(dst => dst.SubscriptionId, opt => opt.Ignore());
+        }
+    }
+}
---
+++ UserAPI/Profiles/UserProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using UserAPI.Models.DTOs;
+
+namespace UserAPI.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDTO>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.UserId))
+                .ForMember(dst => dst.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ReverseMap()
+                .ForMember(dst => dst.UserId, opt => opt.Ignore())
+                .ForMember(dst => dst.FirstName, opt => opt.MapFrom(src => GetFirstName(src.FullName)))
+                .ForMember(dst => dst.LastName, opt => opt.MapFrom(src => GetLastName(src.FullName)));
+        }
+
+        private static string GetFirstName(string fullName)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))
+                return string.Empty;
+            var parts = fullName.Split(' ');
+            return parts.Length > 0 ? parts[0] : string.Empty;
+        }
+
+        private static string GetLastName(string fullName)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))
+                return string.Empty;
+            var parts = fullName.Split(' ');
+            return parts.Length > 1 ? string.Join(" ", parts.Skip(1)) : string.Empty;
+        }
+    }
+}
