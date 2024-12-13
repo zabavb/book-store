deddeb4a768dbc5bb55421c38eb111ab993bec5a
Viktor <bilonizkavik@gmail.com>
2024-12-13 14:21:31 +0100

    Fixed mistake with original and DTO items

---
+++ UserAPI/Models/DTOs/SubscriptionDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace UserAPI.Models.DTOs
+{
+    public class SubscriptionDTO
+    {
+        public Guid? Id { get; set; }
+        public string Title { get; set; }
+
+        public SubscriptionDTO()
+        {
+            Title = string.Empty;
+        }
+        public SubscriptionDTO(Guid? id, string title, string? description, DateTime endDate)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
