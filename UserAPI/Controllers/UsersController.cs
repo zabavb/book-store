780c33801d6d3deb1a3a6cef42d9c9e046842fd5
Viktor <bilonizkavik@gmail.com>
2024-12-13 14:21:53 +0100

    Created UserController

---
+++ UserAPI/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserAPI.Data;
+using UserAPI.Services;
+
+namespace UserAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        
+    }
+}
