d2175200946745bc81344481c3f4796b5a06e5e1
Viktor <bilonizkavik@gmail.com>
2024-12-13 14:23:45 +0100

    Implemented abstraction for CRUD operations in repositories and services

---
+++ UserAPI/Abstractions/IManager.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UserAPI.Models;
+
+namespace UserAPI.Abstractions
+{
+    public interface IManager<T>
+    {
+        Task<PaginatedResult<T>> GetAllEntitiesPaginatedAsync(int pageNumber, int pageSize);
+        Task<T?> GetEntityByIdAsync(Guid id);
+        Task<ICollection<T>> SearchEntitiesAsync(string searchTerm);
+        Task AddEntityAsync(T entity);
+        Task UpdateEntityAsync(T entity);
+        Task DeleteEntityAsync(Guid id);
+    }
+}
