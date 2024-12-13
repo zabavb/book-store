d95dc51908d2ed0ce4bb09a6cd47f3239389452b
Viktor <bilonizkavik@gmail.com>
2024-12-13 14:35:55 +0100

    Implemented necessary services in Program.cs

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using UserAPI.Data;
+using UserAPI.Profiles;
+using UserAPI.Repositories;
+using UserAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<UserDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAutoMapper(typeof(UserProfile));
+builder.Services.AddAutoMapper(typeof(SubscriptionProfile));
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<UserService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
