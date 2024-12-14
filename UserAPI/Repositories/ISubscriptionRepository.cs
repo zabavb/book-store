6f26ccc3ed0dbe96473e5f37f09ed4bc687ec8fb
Viktor <bilonizkavik@gmail.com>
2024-12-14 12:19:04 +0100

    Devided interface IManager on 4, for each service and repository

---
+++ UserAPI/Repositories/ISubscriptionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Library.UserEntities;
+using UserAPI.Models;
+
+namespace UserAPI.Repositories
+{
+    public interface ISubscriptionRepository
+    {
+        Task<PaginatedResult<Subscription>> GetAllEntitiesPaginatedAsync(int pageNumber, int pageSize, string searchTerm);
+        Task<Subscription?> GetEntityByIdAsync(Guid id);
+        Task<IEnumerable<Subscription>> SearchEntitiesAsync(string searchTerm);
+        Task AddEntityAsync(Subscription entity);
+        Task UpdateEntityAsync(Subscription entity);
+        Task DeleteEntityAsync(Guid id);
+    }
+}
---
+++ UserAPI/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using UserAPI.Models.DTOs;
+using UserAPI.Models;
+
+namespace UserAPI.Services
+{
+    public interface IUserService
+    {
+        Task<PaginatedResult<UserDTO>> GetAllEntitiesPaginatedAsync(int pageNumber, int pageSize, string searchTerm, UserFilter? filter);
+        Task<UserDTO?> GetEntityByIdAsync(Guid id);
+        Task AddEntityAsync(UserDTO entity);
+        Task UpdateEntityAsync(UserDTO entity);
+        Task DeleteEntityAsync(Guid id);
+    }
+}
