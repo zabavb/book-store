fee674139c4d88727f62de9b7fc979a4498fb0d0
Viktor <bilonizkavik@gmail.com>
2024-12-15 13:32:38 +0100

    Edited Configuration files due to recent changes with entities

--- UserAPI/Data/Configurations/PasswordConfiguration.cs
+++ UserAPI/Data/Configurations/PasswordConfiguration.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
-using Library.UserEntities;
+using UserAPI.Models;
 
 namespace UserAPI.Data.Configurations
 {
