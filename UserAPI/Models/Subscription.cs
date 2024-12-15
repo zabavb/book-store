714ed2ec29b0dca3c1b49b426d3a8691b5966f8a
Viktor <bilonizkavik@gmail.com>
2024-12-15 13:29:29 +0100

    Now Subscription entity from library is DTO and Subscription from api is the full entity

--- UserAPI/Models/Subscription.cs
+++ UserAPI/Models/Subscription.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Library.UserEntities
+﻿namespace UserAPI.Models
 {
     public class Subscription
     {
-        public Guid Id { get; set; }
+        public Guid SubscriptionId { get; set; }
         public string Title { get; set; }
         public string? Description { get; set; }
         public DateTime EndDate { get; set; }
+        public ICollection<Guid>? UserIds { get; set; }
+        public ICollection<User>? Users { get; set; }
 
         public Subscription()
         {
             Title = string.Empty;
             Description = string.Empty;
             EndDate = DateTime.Now.AddDays(1);
+            Users = new HashSet<User>();
         }
     }
 }
