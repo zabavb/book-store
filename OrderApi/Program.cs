8fc8d192804d721a8c750643574bfcd466234d9f
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-16 22:48:10 +0200

    Added logging to the program

--- OrderApi/Program.cs
+++ OrderApi/Program.cs
@@ -1,17 +1,54 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
 using OrderApi.Data;
 using OrderApi.Services;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 using OrderApi.Repository;
+using Serilog;
+using System.Collections.ObjectModel;
 var builder = WebApplication.CreateBuilder(args);
+
+var columnOptions = new ColumnOptions
+{
+    Store = { StandardColumn.Id, StandardColumn.LogEvent },
+    AdditionalColumns = new Collection<SqlColumn>
+    {
+        new SqlColumn("Properties", System.Data.SqlDbType.NVarChar) {DataLength = -1 }
+    }
+};
+
+var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.MSSqlServer(
+        connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+        sinkOptions: new MSSqlServerSinkOptions
+        {
+            TableName = "Logs",
+            AutoCreateSqlTable = true
+        },
+        columnOptions: columnOptions,
+        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
+        )
+    .Enrich.WithProperty("LogTime",DateTime.UtcNow)
+    .WriteTo.Console(outputTemplate: "{Level}: {Message} - {LogTime:yyyy-MM-dd HH:mm:ss}{NewLine}")
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 // Add services to the container.
 builder.Services.AddDbContext<OrderDbContext>(options => options
         .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<IOrderService, OrderService>();
+
+builder.Services.AddScoped<string>();
+
 builder.Services.AddControllers();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
