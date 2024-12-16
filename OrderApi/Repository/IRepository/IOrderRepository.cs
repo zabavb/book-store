1f27b8e48bcb864dc96b328c41b57a4ff64b34d9
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-16 16:44:53 +0200

    Created Repository Interface

---
+++ OrderApi/Repository/IRepository/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using OrderApi.Models;
+
+internal interface IOrderRepository
+{
+    Task<IEnumerable<Order>> GetAllAsync();
+    Task<Order?> GetByIdAsync(Guid id);
+    Task AddAsync(Order order);
+    Task DeleteAsync(Order order);
+    Task UpdateAsync(Order order);
+}
