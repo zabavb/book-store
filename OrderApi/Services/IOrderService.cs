9108dd754891f3bc5a4669f2d8af28a7e1280bcb
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-16 22:45:09 +0200

    Removed unnecesarry values for UpdateOrder

--- OrderApi/Services/IOrderService.cs
+++ OrderApi/Services/IOrderService.cs
@@ -8,7 +8,7 @@ namespace OrderApi.Services
         Task<PaginatedResult<Order>> GetOrdersAsync(int pageNumber, int pageSize);
         Task<OrderDto> GetOrderByIdAsync(Guid orderId);
         Task<OrderDto> CreateOrderAsync(OrderDto orderDto);
-        Task<OrderDto> UpdateOrderAsync(Guid id, OrderDto orderDto);
+        Task<OrderDto> UpdateOrderAsync(OrderDto orderDto);
         Task<bool> DeleteOrderAsync(Guid id);
     }
 }
