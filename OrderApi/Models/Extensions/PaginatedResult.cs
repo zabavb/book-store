4db7d163845163467c0025e6320c5e0a8c301280
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-16 21:38:05 +0200

    Added paginatedResult extension

---
+++ OrderApi/Models/Extensions/PaginatedResult.cs
@@ -0,0 +1,21 @@
+﻿namespace OrderApi.Models.Extensions
+{
+    public class PaginatedResult<T>
+    {
+        public ICollection<T> Items { get; set; }
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize {  get; set; }
+
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+
+        public PaginatedResult()
+        {
+            Items = new List<T>();
+        }
+
+    }
+}
