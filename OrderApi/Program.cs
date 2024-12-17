362120e0c742756f8aa41c4032f4507e1e0a4b76
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-17 20:34:27 +0200

    Restructured Program.cs to prevent the api from crashing if DB doesn't exist

--- OrderApi/Program.cs
+++ OrderApi/Program.cs
@@ -6,51 +6,56 @@ using OrderApi.Services;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 using OrderApi.Repository;
-using Serilog;
 using System.Collections.ObjectModel;
+using OrderApi.Profiles;
 var builder = WebApplication.CreateBuilder(args);
 
-var columnOptions = new ColumnOptions
+
+
+builder.Services.AddDbContext<OrderDbContext>(options => options
+        .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+/*var columnOptions = new ColumnOptions
 {
     Store = { StandardColumn.Id, StandardColumn.LogEvent },
     AdditionalColumns = new Collection<SqlColumn>
     {
         new SqlColumn("Properties", System.Data.SqlDbType.NVarChar) {DataLength = -1 }
     }
-};
-
-var logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .WriteTo.MSSqlServer(
-        connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
-        sinkOptions: new MSSqlServerSinkOptions
-        {
-            TableName = "Logs",
-            AutoCreateSqlTable = true
-        },
-        columnOptions: columnOptions,
-        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
+};*/
+try
+{
+    var logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.MSSqlServer(
+            connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+            sinkOptions: new MSSqlServerSinkOptions
+            {
+                TableName = "Logs",
+                AutoCreateSqlTable = true
+            },
+            /*        columnOptions: columnOptions,*/
+            restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
         )
-    .Enrich.WithProperty("LogTime",DateTime.UtcNow)
-    .WriteTo.Console(outputTemplate: "{Level}: {Message} - {LogTime:yyyy-MM-dd HH:mm:ss}{NewLine}")
-    .CreateLogger();
+        .Enrich.WithProperty("LogTime", DateTime.UtcNow)
+        .WriteTo.Console(outputTemplate: "{Level}: {Message} - {LogTime:yyyy-MM-dd HH:mm:ss}{NewLine}")
+        .CreateLogger();
 
-builder.Logging.ClearProviders();
-builder.Logging.AddSerilog(logger);
-
-// Add services to the container.
-builder.Services.AddDbContext<OrderDbContext>(options => options
-        .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    builder.Logging.ClearProviders();
+    builder.Logging.AddSerilog(logger);
+}
+catch (Exception ex)
+{
+    throw new Exception("Logger cannot connect to the Database", ex);
+}
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<IOrderService, OrderService>();
-
-builder.Services.AddScoped<string>();
 
 builder.Services.AddControllers();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+/*builder.Services.AddAutoMapper(typeof(OrderProfile));*/
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
