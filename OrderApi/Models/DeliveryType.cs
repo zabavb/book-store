cb5d821b7963bb8a4c4c6a77923063cc916fd0f8
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:16:51 +0200

    Created local Delivery type class

---
+++ OrderApi/Models/DeliveryType.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderApi.Models
+{
+    public class DeliveryType
+    {
+        public Guid DeliveryId { get; set; }
+        public string ServiceName { get; set; }
+
+        public DeliveryType()
+        {
+            ServiceName = string.Empty;
+        }
+    }
+}
