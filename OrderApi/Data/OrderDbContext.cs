27aff19b2bd7e9947cdfbc98be2471919c62a39b
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:50:35 +0200

    Changed Delivery type DbSet name

--- OrderApi/Data/OrderDbContext.cs
+++ OrderApi/Data/OrderDbContext.cs
@@ -8,7 +8,7 @@ namespace OrderApi.Data
     public class OrderDbContext : DbContext
     {
         internal DbSet<Order> Orders { get; private set; } = null!;
-        internal DbSet<DeliveryType> deliveryTypes { get; private set; } = null!;
+        internal DbSet<DeliveryType> DeliveryTypes { get; private set; } = null!;
 
         public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
         {
