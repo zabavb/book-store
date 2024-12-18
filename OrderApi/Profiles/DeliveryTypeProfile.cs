6a67381ce797ccf06af6adf8059d3e3075bf20f0
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:20:23 +0200

    Create Delivery type profile

---
+++ OrderApi/Profiles/DeliveryTypeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OrderApi.Models;
+
+namespace OrderApi.Profiles
+{
+    public class DeliveryTypeProfile : Profile
+    {
+        public DeliveryTypeProfile()
+        {
+            CreateMap<DeliveryType, DeliveryTypeDto>()
+               .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.DeliveryId)).ReverseMap();
+        }
+    }
+}
