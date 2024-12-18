e66cc29c8ae8d848acd414d14f46c0cc7b05a2e4
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:14:45 +0200

    Created delivery type interface

---
+++ OrderApi/Services/IDeliveryTypeService.cs
@@ -0,0 +1,14 @@
+﻿using OrderApi.Models;
+using OrderApi.Models.Extensions;
+
+namespace OrderApi.Services
+{
+    public interface IDeliveryTypeService
+    {
+        Task<PaginatedResult<DeliveryType>> GetDeliveryTypesAsync(int pageNumber, int pageSize);
+        Task<DeliveryTypeDto> GetDeliveryTypeByIdAsync(Guid orderId);
+        Task<DeliveryTypeDto> CreateDeliveryTypeAsync(DeliveryTypeDto deliveryTypeDto);
+        Task<DeliveryTypeDto> UpdateDeliveryTypeAsync(DeliveryTypeDto deliveryTypeDto);
+        Task<bool> DeleteDeliveryTypeAsync(Guid id);
+    }
+}
