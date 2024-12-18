8d8e539799fe1e5609473bbacf22046af33af0b3
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:15:23 +0200

    Added delivery type objects to the seeder

--- OrderApi/Data/DataSeeder.cs
+++ OrderApi/Data/DataSeeder.cs
@@ -1,13 +1,30 @@
-﻿using Library.OrderEntities;
-using Microsoft.EntityFrameworkCore;
-using Order = OrderApi.Models.Order;
-
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApi.Models;
+using OrderStatus = Library.OrderEntities.OrderStatus;
 namespace OrderApi.Data
 {
     public static class DataSeeder
     {
         public static void Seed(ModelBuilder modelBuilder) 
         {
+            var deliveryType1 = new DeliveryType
+            {
+                DeliveryId = Guid.NewGuid(),
+                ServiceName = "Libro"
+            };
+            var deliveryType2 = new DeliveryType
+            {
+                DeliveryId = Guid.NewGuid(),
+                ServiceName = "Ukr Post"
+            };
+            var deliveryType3 = new DeliveryType
+            {
+                DeliveryId = Guid.NewGuid(),
+                ServiceName = "Nova Post"
+            };
+
+            modelBuilder.Entity<DeliveryType>().HasData(deliveryType1, deliveryType2, deliveryType3);
+
             var order1 = new Order
             {
                 OrderId = Guid.NewGuid(),
@@ -17,7 +34,7 @@ namespace OrderApi.Data
                 City = "Lviv",
                 Address = "Address1",
                 Price = (float)100.59,
-                Delivery = DeliveryType.NOVA_POST,
+                DeliveryTypeId = deliveryType1.DeliveryId,
                 DeliveryPrice = (float)120.59,
                 OrderDate = DateTime.Now,
                 DeliveryDate = DateTime.Now.AddDays(2),
@@ -32,7 +49,7 @@ namespace OrderApi.Data
                 City = "Lviv",
                 Address = "Address2",
                 Price = (float)100.59,
-                Delivery = DeliveryType.UKR_POST,
+                DeliveryTypeId = deliveryType2.DeliveryId,
                 DeliveryPrice = (float)120.59,
                 OrderDate = DateTime.Now,
                 DeliveryDate = DateTime.Now.AddDays(7),
@@ -48,7 +65,7 @@ namespace OrderApi.Data
                 City = "Lviv",
                 Address = "Address3",
                 Price = (float)100.59,
-                Delivery = DeliveryType.LIBRO,
+                DeliveryTypeId = deliveryType3.DeliveryId,
                 DeliveryPrice = (float)120.59,
                 OrderDate = DateTime.Now,
                 DeliveryDate = DateTime.Now.AddDays(4),
