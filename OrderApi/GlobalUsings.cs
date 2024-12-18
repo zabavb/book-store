1c58b8d48197f64c51d595f8e9bfc55219f8cf25
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:18:21 +0200

    Added new DTO's

--- OrderApi/GlobalUsings.cs
+++ OrderApi/GlobalUsings.cs
@@ -1,1 +1,4 @@
 ﻿global using OrderDto = Library.OrderEntities.Order;
+global using DeliveryTypeDto = Library.OrderEntities.DeliveryType;
+global using UserDto = Library.UserEntities.User;
+global using BookDto = Library.BookEntities.Book;
