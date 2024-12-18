7accd4ad2c2e53e17bf49c66eb651b45742f0440
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:48:16 +0200

    Updated delivery type service interface

--- OrderApi/Services/IDeliveryTypeService.cs
+++ OrderApi/Services/IDeliveryTypeService.cs
@@ -5,8 +5,8 @@ namespace OrderApi.Services
 {
     public interface IDeliveryTypeService
     {
-        Task<PaginatedResult<DeliveryType>> GetDeliveryTypesAsync(int pageNumber, int pageSize);
-        Task<DeliveryTypeDto> GetDeliveryTypeByIdAsync(Guid orderId);
+        Task<PaginatedResult<DeliveryTypeDto>> GetDeliveryTypesAsync(int pageNumber, int pageSize);
+        Task<DeliveryTypeDto> GetDeliveryTypeByIdAsync(Guid id);
         Task<DeliveryTypeDto> CreateDeliveryTypeAsync(DeliveryTypeDto deliveryTypeDto);
         Task<DeliveryTypeDto> UpdateDeliveryTypeAsync(DeliveryTypeDto deliveryTypeDto);
         Task<bool> DeleteDeliveryTypeAsync(Guid id);
