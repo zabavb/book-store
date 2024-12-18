c0f63949ad41fbb186b94d3f130c1e15c6dac7aa
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:19:46 +0200

    Create Delivery type object class

--- Library/OrderEntities/DeliveryType.cs
+++ Library/OrderEntities/DeliveryType.cs
@@ -2,10 +2,19 @@
 
 namespace Library.OrderEntities
 {
-    public enum DeliveryType
+    public class DeliveryType
     {
-        LIBRO,
-        NOVA_POST,
-        UKR_POST
+        [Required]
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(50, ErrorMessage = "Service name should be less than 50 characters.")]
+        public string ServiceName { get; set; }
+
+        DeliveryType()
+        {
+            ServiceName = string.Empty;
+        }
     }
 }
