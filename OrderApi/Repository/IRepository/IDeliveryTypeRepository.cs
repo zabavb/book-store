5cca9455d335347f6e27c4c7d4be7ec80088fd63
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:19:25 +0200

    Created Delivery type repository interface

---
+++ OrderApi/Repository/IRepository/IDeliveryTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using OrderApi.Models;
+
+namespace OrderApi.Repository.IRepository
+{
+    public interface IDeliveryTypeRepository
+    {
+        Task<IEnumerable<DeliveryType>> GetAllAsync();
+        Task<DeliveryType?> GetByIdAsync(Guid id);
+        Task AddAsync(DeliveryType order);
+        Task DeleteAsync(DeliveryType order);
+        Task UpdateAsync(DeliveryType order);
+    }
+}
