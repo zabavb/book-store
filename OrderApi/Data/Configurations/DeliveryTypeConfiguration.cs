9725c9829d713e36eec6ff2b4d24f3b110df3ed8
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 21:18:58 +0200

    Created delivery type configuration

---
+++ OrderApi/Data/Configurations/DeliveryTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OrderApi.Models;
+
+namespace OrderApi.Data.Configurations
+{
+    internal class DeliveryTypeConfiguration : IEntityTypeConfiguration<DeliveryType>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryType> builder)
+        {
+            builder.Property(d => d.DeliveryId)
+                .HasDefaultValueSql("NEWSEQUENTIALID()");
+
+            builder.Property(d => d.ServiceName)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnType("nvarchar(50)");
+        }
+    }
+}
