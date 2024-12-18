f3e83fc422cb584f2d8b317545994a4284f5f4a4
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-18 22:27:19 +0200

    Added data annotations

--- OrderApi/Models/DeliveryType.cs
+++ OrderApi/Models/DeliveryType.cs
@@ -1,8 +1,14 @@
-﻿namespace OrderApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Models
 {
     public class DeliveryType
     {
+        [Required]
+        [Key]
         public Guid DeliveryId { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(50, ErrorMessage = "Service name should be less than 50 characters.")]
         public string ServiceName { get; set; }
 
         public DeliveryType()
