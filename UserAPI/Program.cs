822b2fd4cc51eedb81b4b1399f37774de26a73b4
Viktor <bilonizkavik@gmail.com>
2024-12-19 17:28:59 +0100

    - Implemented swagger documentations - Fixed implementation of services - Added check if database created before creating table for logging

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -1,42 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using Serilog.Sinks.MSSqlServer;
-using System.Collections.ObjectModel;
-using System.Text;
+using System.Reflection;
 using UserAPI.Data;
 using UserAPI.Profiles;
 using UserAPI.Repositories;
 using UserAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var columnOptions = new ColumnOptions
-{
-    Store = { StandardColumn.Id, StandardColumn.LogEvent },
-    AdditionalColumns = new Collection<SqlColumn>
-    {
-        new SqlColumn("Properties", System.Data.SqlDbType.NVarChar) { DataLength = -1 }
-    }
-};
-
-var logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .WriteTo.MSSqlServer(
-        connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
-        sinkOptions: new MSSqlServerSinkOptions
-        {
-            TableName = "Logs",
-            AutoCreateSqlTable = true
-        },
-        columnOptions: columnOptions,
-        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
-    )
-    .Enrich.WithProperty("LogTime", DateTime.UtcNow)
-    .WriteTo.Console(outputTemplate: "{Level}: {Message} - {LogTime:yyyy-MM-dd HH:mm:ss}{NewLine}")
-    .CreateLogger();
-
-builder.Logging.ClearProviders();
-builder.Logging.AddSerilog(logger);
 
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -50,18 +22,62 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
 builder.Services.AddScoped<ISubscriptionService, SubscriptionService>();
 
-builder.Services.AddScoped<string>();
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "UserAPI",
+        Version = "v1"
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine($"{AppContext.BaseDirectory}", xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+    if (dbContext.Database.EnsureCreated())
+    {
+        try
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.MSSqlServer(
+                    connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+                    sinkOptions: new MSSqlServerSinkOptions
+                    {
+                        TableName = "Logs",
+                        AutoCreateSqlTable = true
+                    },
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
+                )
+                .Enrich.WithProperty("LogTime", DateTime.UtcNow)
+                .WriteTo.Console(outputTemplate: "{Level}: {Message} - {LogTime:yyyy-MM-dd HH:mm:ss}{NewLine}")
+                .CreateLogger();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog(logger);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Logger cannot connect to the Database", ex);
+        }
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "UserAPI");
+    });
 }
 
 app.UseHttpsRedirection();
