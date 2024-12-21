3e06525a0b1c56c546f1290c01b9cf20c2203836
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-21 16:54:07 +0200

    Fixed logger not working if database was not created beforehand

--- OrderApi/Program.cs
+++ OrderApi/Program.cs
@@ -16,32 +16,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<OrderDbContext>(options => options
         .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-/*try
-{
-    var logger = new LoggerConfiguration()
-        .WriteTo.Console()
-        .WriteTo.MSSqlServer(
-            connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
-            sinkOptions: new MSSqlServerSinkOptions
-            {
-                TableName = "Logs",
-                AutoCreateSqlTable = true
-            },
-            *//*        columnOptions: columnOptions,*//*
-            restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
-        )
-        .Enrich.WithProperty("LogTime", DateTime.UtcNow)
-        .WriteTo.Console(outputTemplate: "{Level}: {Message} - {LogTime:yyyy-MM-dd HH:mm:ss}{NewLine}")
-        .CreateLogger();
-
-    builder.Logging.ClearProviders();
-    builder.Logging.AddSerilog(logger);
-}
-catch (Exception ex)
-{
-    throw new Exception("Logger cannot connect to the Database", ex);
-}*/
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 
@@ -50,8 +24,9 @@ builder.Services.AddScoped<IDeliveryTypeService, DeliveryTypeService>();
 
 builder.Services.AddControllers();
 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-/*builder.Services.AddAutoMapper(typeof(OrderProfile));*/
+builder.Services.AddAutoMapper(typeof(OrderProfile));
+builder.Services.AddAutoMapper(typeof(DeliveryTypeProfile));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -66,6 +41,38 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+    if (dbContext.Database.EnsureCreated())
+    {
+        try
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.MSSqlServer(
+                    connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+                    sinkOptions: new MSSqlServerSinkOptions
+                    {
+                        TableName = "Logs",
+                        AutoCreateSqlTable = true
+                    },
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
+                )
+                .Enrich.WithProperty("LogTime", DateTime.UtcNow)
+                .WriteTo.Console(outputTemplate: "{Level}: {Message} - {LogTime:yyyy-MM-dd HH:mm:ss}{NewLine}")
+                .CreateLogger();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog(logger);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Logger cannot connect to the Database", ex);
+        }
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
