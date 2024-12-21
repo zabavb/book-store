19c9cb1c2eeb457ae82813aea33e561da34ec764
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-21 17:21:40 +0200

    Added Order filter extension

---
+++ OrderApi/Models/Extensions/OrderFilter.cs
@@ -0,0 +1,15 @@
+﻿using Library.OrderEntities;
+using Microsoft.Identity.Client;
+
+namespace OrderApi.Models.Extensions
+{
+    public class OrderFilter
+    {
+        public DateTime? OrderDateStart { get; set; }
+        public DateTime? OrderDateEnd { get; set; }
+        public DateTime? DeliveryDateStart { get; set; }
+        public DateTime? DeliveryDateEnd { get; set; }
+        public OrderStatus? Status { get; set; }
+        public Guid? DeliveryId { get; set; }
+    }
+}
