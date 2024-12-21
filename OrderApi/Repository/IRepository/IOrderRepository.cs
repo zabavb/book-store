d92c59f9adacd08aa3af136db8f255952c3fc7d2
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-21 17:37:27 +0200

    Added input values for filtering

--- OrderApi/Repository/IRepository/IOrderRepository.cs
+++ OrderApi/Repository/IRepository/IOrderRepository.cs
@@ -3,7 +3,7 @@ using OrderApi.Models.Extensions;
 
 public interface IOrderRepository
 {
-    Task<PaginatedResult<Order>> GetAllPaginatedAsync(int pageNumber, int pageSize);
+    Task<PaginatedResult<Order>> GetAllPaginatedAsync(int pageNumber, int pageSize, string searchTerm, OrderFilter? filter);
     Task<Order?> GetByIdAsync(Guid id);
     Task AddAsync(Order order);
     Task DeleteAsync(Order order);
