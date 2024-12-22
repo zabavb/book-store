5d0bd34fcf9bc39ce540b58d23a05a10dd1b8d1a
Viktor <bilonizkavik@gmail.com>
2024-12-22 19:56:07 +0100

    Replaced UserFilter from UserAPI to Library

---
+++ Library/Filters/UserFilter.cs
@@ -0,0 +1,17 @@
+﻿using Library.UserEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Filters
+{
+    public class UserFilter
+    {
+        public DateTime? DateOfBirthStart { get; set; }
+        public DateTime? DateOfBirthEnd { get; set; }
+        public RoleType? Role { get; set; }
+        public bool HasSubscription { get; set; }
+    }
+}
