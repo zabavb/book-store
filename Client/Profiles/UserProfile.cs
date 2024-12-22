f899056ac7d07b23712ea8693d1287dbbac1d4e9
Viktor <bilonizkavik@gmail.com>
2024-12-22 20:01:33 +0100

    Created profile for user entity

---
+++ Client/Profiles/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Client.Models.User;
+using Library.UserEntities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Client.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, ManageUserViewModel>();
+        }
+    }
+}
