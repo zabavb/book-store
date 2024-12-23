272b1b8f83e9882101d7e93ce8eb77e1e4e73bdf
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-23 20:08:49 +0200

    Created order profile

---
+++ Client/Profiles/OrderProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Client.Models.OrderEntities.Order;
+using Library.OrderEntities;
+
+namespace Client.Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile() 
+        {
+            CreateMap<Order, ManageOrderViewModel>();
+        }
+    }
+}
