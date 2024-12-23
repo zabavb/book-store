8c7e3df6f6d4cd0ed517891b5f959634b0072335
Viktor <bilonizkavik@gmail.com>
2024-12-23 14:24:57 +0100

    Implemented AuthController

---
+++ Client/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using Client.Models.Extensions;
+using Library.UserEntities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Client.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseAddress;
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthController(HttpClient httpClient, IOptions<JwtSettings> jwtOptions)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _baseAddress = "https://localhost:7007/gateway/users";
+            _jwtSettings = jwtOptions.Value;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // HTTP request to PasswordsController/auth
+            var user = new User();// ValidateUser(request.FullName, request.Password);
+
+            if (user == null)
+                return Unauthorized("Invalid username or password.");
+
+            var token = GenerateJwtToken(user);
+            return Ok(new
+            {
+                Token = token,
+                ExpiresInMinutes = _jwtSettings.ExpiresInMinutes
+            });
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, $"{user.FirstName} {user.LastName}"),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiresInMinutes),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
