cebd845721777184b3a265fcb7800ecdf1d8c3f6
Viktor <bilonizkavik@gmail.com>
2024-12-23 14:25:15 +0100

    Added JwtBearer

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Client.Models.Extensions;
 using Client.Profiles;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]!))
+        };
+    });
 builder.Services.AddAutoMapper(typeof(UserProfile));
 builder.Services.AddScoped<HttpClient>();
 
@@ -12,14 +31,11 @@ builder.Services.AddControllersWithViews()
         options.ViewLocationFormats.Add("/Client/Views/{1}/{0}.cshtml");
     });
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
