bdf950fb2a5d6cd90373798521eeb06bde817b33
Viktor <bilonizkavik@gmail.com>
2024-12-23 13:46:45 +0100

    Created helpers for AuthController

---
+++ Client/Models/Extensions/JwtSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Client.Models.Extensions
+{
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int ExpiresInMinutes { get; set; }
+
+        public JwtSettings()
+        {
+            SecretKey = null!;
+            Issuer = null!;
+            Audience = null!;
+        }
+    }
+}
---
+++ Client/Models/Extensions/LoginRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models.Extensions
+{
+    public class LoginRequest
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public LoginRequest()
+        {
+            FullName = null!;
+            Password = null!;
+        }
+    }
+}
