a3ac76e9484601c32a15be5a50d0625b94aca637
Viktor <bilonizkavik@gmail.com>
2024-12-23 10:26:32 +0100

    Renamed all actions in such way that all "Entity" and "Entities" are removed from names of actions for simplicity

--- UserAPI/Services/ISubscriptionService.cs
+++ UserAPI/Services/ISubscriptionService.cs
@@ -4,10 +4,10 @@ namespace UserAPI.Services
 {
     public interface ISubscriptionService
     {
-        Task<PaginatedResult<SubscriptionDto>> GetAllEntitiesPaginatedAsync(int pageNumber, int pageSize, string searchTerm);
-        Task<SubscriptionDto?> GetEntityByIdAsync(Guid id);
-        Task AddEntityAsync(SubscriptionDto entity);
-        Task UpdateEntityAsync(SubscriptionDto entity);
-        Task DeleteEntityAsync(Guid id);
+        Task<PaginatedResult<SubscriptionDto>> GetAllAsync(int pageNumber, int pageSize, string searchTerm);
+        Task<SubscriptionDto?> GetByIdAsync(Guid id);
+        Task AddAsync(SubscriptionDto entity);
+        Task UpdateAsync(SubscriptionDto entity);
+        Task DeleteAsync(Guid id);
     }
 }
