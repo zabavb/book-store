4fab6a7059e104dcdf3ae0ad339945d86108ea0d
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2024-12-23 20:11:32 +0200

    Created Delivery type viewmodel

---
+++ Client/Models/OrderEntities/DeliveryType/ManageDeliveryTypeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models.OrderEntities.DeliveryType
+{
+    public class ManageDeliveryTypeViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(AllowEmptyStrings = false,ErrorMessage = "Service name is required.")]
+        [StringLength(50, ErrorMessage = "Service name should be less than 50 characters.")]
+        public string ServiceName { get; set; }
+
+        public ManageDeliveryTypeViewModel()
+        {
+            ServiceName = string.Empty;
+        }
+    }
+}
