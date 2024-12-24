fc79c7c52d7c0885ea841a1a74fc772c180eb696
Bohdan <bogdandibskij@gmail.com>
2024-12-24 23:20:37 +0200

    Add db configuration for Publisher entity

---
+++ BookApi/Data/Configuration/PublisherConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Models;
+
+namespace BookApi.Data.Configuration
+{
+    internal class PublisherConfiguration : IEntityTypeConfiguration<Publisher>
+    {
+        public void Configure(EntityTypeBuilder<Publisher> builder)
+        {
+            builder.Property(p => p.Id)
+                   .HasDefaultValueSql("NEWID()");
+        }
+    }
+}
