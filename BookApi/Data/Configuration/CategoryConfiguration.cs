3715c672b16e6324ada29b7e7ebde6643db8d728
Bohdan <bogdandibskij@gmail.com>
2024-12-24 23:13:47 +0200

    Add db configuration for Category entity

---
+++ BookApi/Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Models;
+
+namespace BookApi.Data.Configuration
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(c => c.Id)
+                   .HasDefaultValueSql("NEWID()");
+        }
+    }
+}
