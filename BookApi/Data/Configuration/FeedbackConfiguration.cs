b39520b9c2e4f8ea0c9a46725bd73afce80490ad
Bohdan <bogdandibskij@gmail.com>
2024-12-24 23:20:06 +0200

    Add db configuration for Feedback entity

---
+++ BookApi/Data/Configuration/FeedbackConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Models;
+
+namespace BookApi.Data.Configuration
+{
+    internal class FeedbackConfiguration : IEntityTypeConfiguration<Feedback>
+    {
+        public void Configure(EntityTypeBuilder<Feedback> builder)
+        {
+            builder.Property(f => f.Id)
+                   .HasDefaultValueSql("NEWID()");
+        }
+    }
+}
