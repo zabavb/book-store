96b99c29f59d44cdbecd0ea0d778e2ea0bc9070f
Viktor <bilonizkavik@gmail.com>
2024-12-24 20:20:07 +0100

    Implemented interface IManagable which Includes CUD operations (R - Read are omitted, because each service and repository has different parameters)

---
+++ Library/Interfaces/IManagable.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Interfaces
+{
+    public interface IManagable<T>
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(Guid id);
+    }
+}
