a34ecddc52933ec808e9d95f62e0a3ea9c11cf3c
Bohdan <bogdandibskij@gmail.com>
2024-12-24 23:11:47 +0200

    Add db configuration for Book entity

---
+++ BookApi/Data/Configuration/BookConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Models;
+
+namespace BookApi.Data.Configuration
+{
+    internal class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.Property(b => b.Id)
+                   .HasDefaultValueSql("NEWSEQUENTIALID()");
+        }
+    }
+}
