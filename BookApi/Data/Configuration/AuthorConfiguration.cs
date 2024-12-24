d3681e1cacd27a6e420963b5b49e0cd27eedf52d
Bohdan <bogdandibskij@gmail.com>
2024-12-24 23:19:25 +0200

    Add db configuration for Author entity

---
+++ BookApi/Data/Configuration/AuthorConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Models;
+
+namespace BookApi.Data.Configuration
+{
+    internal class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.Property(a => a.Id)
+                   .HasDefaultValueSql("NEWID()");
+        }
+    }
+}
